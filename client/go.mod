b036e7f38a840a6a533804f1c4bd1fa2cb86bc36
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2021-09-27 17:23:59 +0200

    gbng

--- client/go.mod
+++ client/go.mod
@@ -3,6 +3,7 @@ module github.com/lorenzotinfena/cp-frontier
 go 1.17
 
 require (
+	github.com/gomarkdown/markdown v0.0.0-20210918233619-6c1113f12c4a // indirect
 	github.com/shurcooL/go v0.0.0-20200502201357-93f07166e636 // indirect
 	github.com/shurcooL/go-goon v0.0.0-20210110234559-7585751d9a17 // indirect
 	github.com/shurcooL/goexec v0.0.0-20200425235707-36ff6d2d1adc // indirect
--- client/main.go
+++ client/main.go
@@ -3,22 +3,35 @@ package main
 import (
 	"fmt"
 	"syscall/js"
+	"github.com/gomarkdown/markdown/parser"
+	"github.com/gomarkdown/markdown"
 )
 
 func main() {
 	fmt.Println("....")
+
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+	pa := parser.NewWithExtensions(extensions)
+
+
+	
+	md := []byte(`
+	## markdown document
+	
+	# test
+	
+	ulla
+	
+	- fdsaf
+	- fdafdsf
+	`)
+	h := string(markdown.ToHTML(md, pa, nil))
+	fmt.Println("------------------")
+	fmt.Println(h)
 	document := js.Global().Get("document")
 	p := document.Call("createElement", "p")
-	p.Set("innerHTML", "HElofesfesf")
-	p.Set("className", "block")
+	p.Set("innerHTML", h)
 
-	styles := document.Call("createElement", "style")
-	styles.Set("innerHTML", `
-		.block {
-			border: 1px solid black; color: red; bakground: green;
-		}
-	
-	`)
-	document.Get("head").Call("appendChild", styles)
+
 	document.Get("body").Call("appendChild", p)
 }
